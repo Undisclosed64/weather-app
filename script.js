6dc32020c9b196ddec1646c5fb2b36614539dfcf
Undisclosed <undisclosed674@gmail.com>
2021-08-11 04:23:22 -0700

    add func for returning needed data as obj

--- script.js
+++ script.js
@@ -13,5 +13,22 @@ searchBtn.addEventListener('click', () => {
 const searchValue = searchInput.value;
 const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=7632a408d6b0f55924e9958de6220968&q=${searchValue}`,{ mode: 'cors'});
 const responseData = await response.json();
-console.log(responseData);
+console.log(responseData);    
+processFetchedData(responseData);
 };
+
+ function processFetchedData(data){
+     const dataObj = {
+        temp: data.main['temp'],
+        feelsLike: data.main['feels_like'],
+        humidity: data.main['humidity'],
+        locationName: data.name,
+        countryName: data.sys.country,
+        weatherDescription: data.weather[0]['description'],
+        icon: data.weather[0]['icon']
+     };
+     console.log(dataObj);
+/*console.log(data.main['temp'],data.main['feels_like'],data.main['humidity'],data.name,data.sys.country,data.weather[0]['description'],data.weather[0]['icon']);*/
+}
+
+
