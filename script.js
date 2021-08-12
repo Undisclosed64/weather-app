e127a262e8779cf71e4e9fb26b0fd2d69d875cce
Undisclosed <undisclosed674@gmail.com>
2021-08-12 07:03:41 -0700

    update file

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 //bismillah
 //select necessary DOM elements
 const searchInput = document.querySelector('input');
 const searchBtn = document.querySelector('.search_btn');
+const container = document.querySelector('.container');
 
 //add event listener to search button
 searchBtn.addEventListener('click', () => { 
@@ -11,7 +13,7 @@ searchBtn.addEventListener('click', () => {
  async function callAPI(){
 //get search input value
 const searchValue = searchInput.value;
-const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=7632a408d6b0f55924e9958de6220968&q=${searchValue}`,{ mode: 'cors'});
+const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?APPID=7632a408d6b0f55924e9958de6220968&q=${searchValue}&units=metric`,{ mode: 'cors'});
 const responseData = await response.json();
 console.log(responseData);    
 processFetchedData(responseData);
@@ -24,11 +26,41 @@ processFetchedData(responseData);
         humidity: data.main['humidity'],
         locationName: data.name,
         countryName: data.sys.country,
+        weatherMain:data.weather[0]['main'],
         weatherDescription: data.weather[0]['description'],
         icon: data.weather[0]['icon']
      };
      console.log(dataObj);
-/*console.log(data.main['temp'],data.main['feels_like'],data.main['humidity'],data.name,data.sys.country,data.weather[0]['description'],data.weather[0]['icon']);*/
-}
+     createDOMElements(dataObj);
+    }
+    function createDOMElements(location){
+        //select lhs,lhs content
+        const lhs = document.querySelector('.lhs');
+        const lhsContent = document.querySelector('.lhs-content');
 
+        //handle temperature
+        const temperature = document.querySelector('.temperature');
+         const tempDeg = document.querySelector('.tempDeg');
+         temperature.innerText = Math.round(location.temp);
+         temperature.appendChild(tempDeg);
+
+         //handle location name
+         const name = document.querySelector('.location');
+         name.innerText = location.locationName;
+        /* const dateParent = document.querySelector('.date');
+         console.log(dateParent);
+         const date = new Date();
+         dateParent.appendChild(date);*/
+         
+         //handle weather info
+         const weatherInfo = document.querySelector('.weather-desc');
+         weatherInfo.innerText = location.weatherMain;
+
+
+       
+
+   
+};
+   
+/*console.log(data.main['temp'],data.main['feels_like'],data.main['humidity'],data.name,data.sys.country,data.weather[0]['description'],data.weather[0]['icon']);*/
 
